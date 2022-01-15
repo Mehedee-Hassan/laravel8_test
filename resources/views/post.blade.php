1c3079f045e63685d5e3b88fa2b01128d0d69b68
mehedee-hassan <razin.meah@gmail.com>
2022-01-15 18:43:16 +0900

    t8:add post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -19,15 +19,8 @@
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <h2><a href="/post">Blog Post 2</a></h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+
+        <a href="/"> <-HOME </a>
 
         <script type="script/js" src="./app.js"></script>
     </body>
